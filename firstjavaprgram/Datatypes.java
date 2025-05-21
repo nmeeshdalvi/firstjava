8938538acb55dab444ebd2ebd55fe040d0fb2c2e
nmeeshdalvi <nmeeshdalvi@gmail.com>
2025-05-21 09:39:32 +0000

    datatype

---
+++ firstjavaprgram/Datatypes.java
@@ -0,0 +1,38 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        //boolean examle
+        boolean Iot = true;
+        System.out.println(Iot);
+
+        //byte example
+        byte range;
+        range =127;
+        System.out.println(range);
+
+        //short example 
+        short range1;
+        range1 = -127;
+        System.out.println(range1);
+
+        //int example
+        int range2 =1234567890;
+        System.out.println(range2);
+
+        //long example
+        long range3 = 555674955555l;
+        System.out.println(range3);
+
+        //double example(decimal number)
+        double number = 99.9;
+        System.out.println(number);
+
+        //float example
+        float number1 = 99.7232f;
+        System.out.println(number1);
+
+        //char example (Unicode value of A is '\u0041')
+        char letter = '\u0041';
+        System.out.println(letter);
+
+    }
+}
---
+++ firstjavaprgram/Firstjava.java
@@ -0,0 +1,17 @@
+public class Firstjava {
+    
+    public static void main(String[] args){
+
+      String name = "Nmeesh";
+      int age =21;
+      int mob = 789034567;
+      String city = "Kalyan";
+      String gender = "Male";
+
+      System.out.println(name);
+      System.out.println(age);
+      System.out.println(mob);
+      System.out.println(city);
+      System.out.println(gender);
+    } 
+}
