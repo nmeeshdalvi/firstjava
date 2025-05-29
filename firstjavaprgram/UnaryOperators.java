50045d74eeac67231ca9879b23e24de508da779e
nmeeshdalvi <nmeeshdalvi@gmail.com>
2025-05-29 10:23:46 +0000

    Opearators

---
+++ firstjavaprgram/UnaryOperators.java
@@ -0,0 +1,25 @@
+public class UnaryOperators {
+    public static void main(String[] args) {
+        int num1 = 10;
+    int num2 = 20;
+
+    double num = 10, resultNumber;
+    boolean flag = true;
+
+    System.out.println("-num1 = " + +num1);//number is equal to 10
+
+    System.out.println("-num2 = " + -num2);//
+
+    System.out.println("++num = " + ++num);//number is equal to 11
+
+    System.out.println("--num = " + --num);//number is equal to 10
+
+    System.out.println("!flag = " + !flag);//flag is equal to false
+
+    System.out.println(num++);
+    System.out.println(num);
+
+    System.out.println(++num);
+    System.out.println(num);
+    }
+}
